b8acccfc10115c295fbc1d721a973c04a4b7574e
Samabro95 <146076908+Samabro95@users.noreply.github.com>
2023-10-12 11:27:43 +0200

    task 4 till javascript events

--- javascriptEvents/main.js
+++ javascriptEvents/main.js
@@ -1,24 +1,28 @@
+/*Ändra bild slumpmässigt
+  - Lägg till en array med bildfiler i Javascript
+*/
+
 
         
         // Skapa en variabel för varje button
-        const hideButton1 = document.getElementById("hideButton1");
-        const hideButton2 = document.getElementById("hideButton2");
-        const hideButton3 = document.getElementById("hideButton3")
+      const hideButton1 = document.getElementById("hideButton1");
         
         
-        const widthButton = document.getElementById("widthButton");
-       
+        
+      const widthButton = document.getElementById("widthButton1");
+       // skapa en variable för byta bild
+       const changeImageButton = document.getElementById("changeImage");
+
+       const kitten = document.getElementById("image1");
+
+
        // En funktion för att visa bilden
        function showHideImage(){
-         const buttonId = this.id;
-         const kitten = document.querySelector("#" + buttonId + " + button + img");
-         console.log(kitten);
+        
          
-         //const kitten = document.getElementById("imageId");
-         //Skapa en variabel som berättar om bilden syns eller ej
          const kittenIsVisible = kitten.style.display === "block";
          //Skapa en if-sats som gör motsatsen till syns eller dold.
-         if(kittenIsVisible){
+          if(kittenIsVisible){
             kitten.style.display = "none";
          } else {
             kitten.style.display = "block";
@@ -30,14 +34,50 @@
     
        // En funktion för att ändra bildens bredd
        function changeWidthImage(){
-          const kitten = document.querySelector("img");
-          kitten.width = 550;
+          const kitten = document.getElementById("image1");
+          kitten.width = 500;
        }
+       // En funktion som byter ut bilden
+       function changeImageFunction(){
+          const allImages = [
+             {
+                src:"img1.jpg",
+                alt:"",
+                getSrc: function(){
+                  return this.src;
+               }
+             },
+             {
+               src:"img2.jpg",
+               alt:"",
+               getSrc: function(){
+                 return this.src;
+              }
+            },
+            {
+               src:"img3.jpg",
+               alt:"",
+               getSrc: function(){
+                 return this.src;
+              }
+            }
+            
+           ];
+          
+          const getRandomInt = function(max) {
 
+            return Math.floor(Math.random() * max);
+          } 
+
+          const choosenImage = allImages[getRandomInt(allImages.length)];
+          kitten.src = choosenImage.getSrc();
+          kitten.alt = choosenImage.alt; 
+          
+         
+       }
+       
        // Koppla en eventListener till varje button
        hideButton1.addEventListener("click", showHideImage);
-       hideButton2.addEventListener("click", showHideImage);
-       hideButton3.addEventListener("click", showHideImage);
-
        widthButton.addEventListener("click" , changeWidthImage);
+       changeImageButton.addEventListener("click", changeImageFunction)
   
